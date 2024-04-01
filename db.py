2689d40e01bcbc7ac2486bb9cec1ca7facd43fb1
shashankpandey04 <jpshashank200@gmail.com>
2024-04-01 16:15:19 +0530

    Fixed db.py connection.

--- db.py
+++ db.py
@@ -1,14 +1,8 @@
 import mysql.connector as mysql
-try:
-    mycon = mysql.connect(
+mycon = mysql.connect(
         host='cynibot_cyni',
         user='mysql',
         passwd='995ef7a66cb1feb880bd',
         database='cyni'
-    )
-    mycur = mycon.cursor()
-except mysql.Error as err:
-    print("Error connecting to database:", err)
-finally:
-    if mycon:
-        mycon.close()
+)
+mycur = mycon.cursor()
