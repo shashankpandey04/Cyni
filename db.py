f7ca2496f6e98473b80d1af2575511f7c8ee5e59
shashankpandey04 <jpshashank200@gmail.com>
2024-04-01 12:56:39 +0530

    Added MySQL Connector Debug

--- db.py
+++ db.py
@@ -1,3 +1,14 @@
-import mysql.connector as msc
-mycon=msc.connect(host='cynibot_cyni',user='mysql',passwd='995ef7a66cb1feb880bd',database='cyni')
-mycur=mycon.cursor()
+import mysql.connector as mysql
+try:
+    mycon = mysql.connect(
+        host='cynibot_cyni',
+        user='mysql',
+        passwd='995ef7a66cb1feb880bd',
+        database='cyni'
+    )
+    mycur = mycon.cursor()
+except mysql.Error as err:
+    print("Error connecting to database:", err)
+finally:
+    if mycon:
+        mycon.close()
