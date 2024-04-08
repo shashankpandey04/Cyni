7691eb3028a70d0f72e6a68ce98b86caf523cbaf
shashankpandey04 <jpshashank200@gmail.com>
2024-04-08 12:17:02 +0530

    Running Cyni Beta

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-#from cyni import cyni
+from cyni import cyni
 
-#if __name__ == '__main__':
-#    cyni()
-
-import subprocess
-try:
-    subprocess.run(["pip", "install", "--upgrade", "mysql-connector-python"])
-    print("mysql-connector-python upgraded successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Error: {e}")
+if __name__ == '__main__':
+    cyni()
