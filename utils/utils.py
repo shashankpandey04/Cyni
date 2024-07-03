693b51e06df0ec0008c8c38e715f47b53e2ef8b4
shashankpandey04 <jpshashank200@gmail.com>
2024-07-03 18:31:03 +0530

    Updated utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -14,7 +14,10 @@ async def get_prefix(bot, message):
     """
     try:
         settings = await bot.settings.get(message.guild.id)
-        return settings["customization"]["prefix"]
+        try:
+            return settings["customization"]["prefix"]
+        except KeyError:
+            return commands.when_mentioned_or("?")(bot, message)
     except KeyError:
         return commands.when_mentioned_or("?")(bot, message)
     
