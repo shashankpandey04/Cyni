df2d6c254a8e71ac9b7b20dc1f9dd457c30c74b8
Shashank Pandey <jpshashank200@gmail.com>
2025-02-05 14:36:04 +0530

    Removed CAD Routes [Will add in dashboard.py]

--- dashboard.py
+++ dashboard.py
@@ -1020,4 +1020,4 @@ def run_production():
     serve(app, host='0.0.0.0', port=80)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',port=80)
