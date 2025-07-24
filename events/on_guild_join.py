11bea4016cce7701d4a3da907c32d7074d15be7c
Shashank Pandey <jpshashank200@gmail.com>
2025-07-24 10:31:20 +0530

    Updated guild join event

--- events/on_guild_join.py
+++ events/on_guild_join.py
@@ -10,6 +10,12 @@ from utils.utils import discord_time, generate_embed
 class OnGuildJoin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.guild_log_channel = None
+
+    async def cog_load(self):
+        log_guild = self.bot.get_guild(1152949579407442050)
+        if log_guild:
+            self.guild_log_channel = log_guild.get_channel(1210248878599839774)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -17,12 +23,30 @@ class OnGuildJoin(commands.Cog):
         This event is triggered when the bot joins a guild.
         :param guild (discord.Guild): The guild that the bot joined.
         """
+
+        if self.bot.is_premium:
+            premium = await self.bot.premium.find_by_id(guild.id)
+            if not premium:
+                embed = discord.Embed(
+                    title="⚠️ Your server is not premium!",
+                    description="Please upgrade to premium to use this bot.",
+                    color=discord.Color.red()
+                )
+                embed.set_footer(text="CYNI Premium")
+                await guild.owner.send(embed=embed)
+                await guild.leave()
+                return
         
-        log_guild = self.bot.get_guild(1152949579407442050)
-        guild_log_channel = log_guild.get_channel(1210248878599839774)
+        if not self.guild_log_channel:
+            log_guild = self.bot.get_guild(1152949579407442050)
+            if log_guild:
+                self.guild_log_channel = log_guild.get_channel(1210248878599839774)
+        if not self.guild_log_channel:
+            return
+        
         created_at = discord_time(guild.created_at)
         embed = generate_embed(
-            guild.id,
+            guild,
             title=f"Joined {guild.name}",
             category="logging",
             description=f"""
@@ -39,9 +63,7 @@ class OnGuildJoin(commands.Cog):
             embed.set_thumbnail(url=guild.icon_url)
         except:
             pass
-        await guild_log_channel.send(
-            embed=embed
-        )
+        await self.guild_log_channel.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(OnGuildJoin(bot))
